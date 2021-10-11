d6780adb02f5a76f2d70022a34c05ae7975f304b
demin1078 <43730344+demin1078@users.noreply.github.com>
2021-10-11 11:21:34 +0300

    Update 3.2.py

--- Programming/3.2.py
+++ Programming/3.2.py
@@ -3,5 +3,6 @@ arr = []
 for i in range(random.randint(4,10)):
     arr.append(random.randint(1,20))
 
+print("Сгенерированный массив:" + ",".join(map(str, arr)))
 arrSet = set(arr)
 print("Массив без повторяющихся элементов: " + ",".join(map(str,arrSet)))
